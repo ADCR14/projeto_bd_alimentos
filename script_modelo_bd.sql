e35e587daa52031d64a4604c735b2d2b8d76badc
ADCR14 <160253285+ADCR14@users.noreply.github.com>
2024-08-02 12:08:37 -0300

    alterados

--- script_modelo_bd.sql
+++ script_modelo_bd.sql
@@ -4,55 +4,50 @@ use estoque_alimentos;
 
 create table alimento(
 	pk int not null auto_increment, primary key(pk), 
-    nome varchar(255) not null,
-    qtd int not null,
-    perecivel  decimal(65) not null,
-    vencimento int not null,
-    lote_fk int not null, foreign key (lote_fk) references lote (pk),
-    estoque_fk int not null, foreign key (estoque_fk) references estoque (pk),
-    local_armazenamento_fk int not null, foreign key (local_armazenamento_fk) references local_armazenamento (pk)
+    nome varchar(255) not null
 );
 
 create table lote(
 	pk int not null auto_increment, primary key(pk), 
-    numeracao_lote varchar(255) not null
+    quantidade INT not null,
+    data_vencimento date not null,
+    codigo varchar(255) not null,
+	pedido_fk int not null, foreign key (pedido_fk) references pedido (pk),
+    alimento_fk int not null, foreign key (alimento_fk) references alimento (pk)
 );
-
-create table estoque(
-	pk int not null auto_increment, primary key(pk), 
-    capacidade int not null,
-    pedido_fk int not null, foreign key (pedido_fk) references pedido (pk)
-);
-
-create table local_armazenamento(
-	pk int not null auto_increment, primary key(pk), 
-    nome varchar(255) not null,
-    prateleira varchar(45),
-    freezer varchar(45),
-    geladeira varchar(45)
-);
-
 
 create table detalhe_pedido(
 	pk int not null auto_increment, primary key(pk), 
-    nome varchar(255) not null,
-    qtd int not null,
-    valorbase int not null,
-    valortotal int not null,
-    gastototal int not null
+    quantidade_solicitada int not null,
+    valor double not null,
+    status_entrega tinyint not null
 );
 
 create table pedido(
 	pk int not null auto_increment, primary key(pk), 
-    cabecalho varchar(255) not null,
+    origem varchar(255) not null,
+    data_pedido date not null,
+    data_entrega date not null,
     detalhe_pedido_fk int not null, foreign key (detalhe_pedido_fk) references detalhe_pedido (pk)
 );
 
 
-insert into lote (numeracao_lote) values ("1234567");
-insert into lote (numeracao_lote) values ("1234578");
-insert into lote (numeracao_lote) values ("1234589");
-insert into lote (numeracao_lote) values ("1234510");
+insert into alimento (nome) values ("leite");
+insert into alimento (nome) value ("chá");
+insert into alimento (nome) value ("pão");
+insert into alimento (nome) value ("manteiga");
+insert into alimento (nome) value ("queijo");
+
+select * from detalhe_pedido;
+
+insert into detalhe_pedido (quantidade_solicitada, valor, status_entrega) value (2, 27.50, "1");
+insert into detalhe_pedido (quantidade_solicitada, valor, status_entrega) value (10, 30.00, "0");
+insert into detalhe_pedido (quantidade_solicitada, valor, status_entrega) value (14, 50.10, "1");
+insert into detalhe_pedido (quantidade_solicitada, valor, status_entrega) value (2, 27.50, "0");
+insert into detalhe_pedido (quantidade_solicitada, valor, status_entrega) value (5, 60.50, "1");
+
+
+
 
 
 
